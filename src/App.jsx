73f79154a62151081969d23dab43721a68d169f4
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-02-15 05:29:23 +0900

    wqcoimerp

--- src/App.jsx
+++ src/App.jsx
@@ -23,28 +23,38 @@
 // };
 
 // export default App;
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import GoogleCallback from "./GoogleCallback"; 
 import logo from "./assets/logo.png";
 import "./App.css";
+
+const Home = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const code = searchParams.get("code");
+
+  return (
+    <div className="container">
+      {/* ✅ URL에 "code"가 있으면 GoogleCallback 실행 */}
+      {code ? <GoogleCallback /> : null}
+
+      <div className="card">
+        <img src={logo} alt="앱 로고" className="app-logo" />
+        <h1>인증 성공!</h1>
+        <p>브라우저를 닫고 앱으로 돌아가기</p>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* ✅ 메인 페이지 */}
-        <Route path="/" element={
-          <div className="container">
-            <div className="card">
-              <img src={logo} alt="앱 로고" className="app-logo" />
-              <h1>인증 성공!</h1>
-              <p>브라우저를 닫고 앱으로 돌아가기</p>
-            </div>
-          </div>
-        } />
+        {/* ✅ 루트("/")에서도 GoogleCallback을 실행할 수 있도록 Home 컴포넌트 사용 */}
+        <Route path="/" element={<Home />} />
 
-        {/* ✅ OAuth 인증 후 GoogleCallback 실행 */}
+        {/* ✅ 개별적으로 /callback 경로도 추가 가능 */}
         <Route path="/callback" element={<GoogleCallback />} />
       </Routes>
     </Router>
@@ -52,3 +62,4 @@ const App = () => {
 };
 
 export default App;
+
